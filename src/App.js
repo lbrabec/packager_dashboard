3052f2f6726d5f7f1e8b755334b954660b871fce
Lukas Brabec <lbrabec@redhat.com>
2020-05-15 12:53:24 +0200

    accept new data format

--- src/App.js
+++ src/App.js
@@ -16,18 +16,31 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
+  getData() {
     //fetch('http://localhost:5000/api/v1/packager_dashboard/churchyard')
-    fetch('http://192.168.0.87:5000/api/v1/packager_dashboard/churchyard')
+    //fetch('http://192.168.0.87:5000/api/v1/packager_dashboard/churchyard')
     //fetch('http://localhost:5000/api/v1/packager_dashboard/dajt')
+    fetch('http://localhost:5000/api/v1/packager_dashboard/frantisekz')
     .then((response) => response.json())
     .then((data) => {
         console.log('Success:', data);
         this.setState({user_data: data})
+        if(data.bzs.status === 200 && data.prs.status === 200 && data.static_info.status === 200){
+          clearInterval(this.interval);
+        }
     })
     .catch((error) => {
         console.error('Error:', error);
     });
+  }
+
+  componentDidMount() {
+    this.getData()
+    this.interval = setInterval(() => this.getData(), 10000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
@@ -35,27 +48,39 @@ class App extends Component {
       return ('Loading')
     }
 
-    const { bugs, orphans, packages, pull_requests, updates, overrides } = this.state.user_data
+    //const { bugs, orphans, packages, pull_requests, updates, overrides } = this.state.user_data
+    const { bzs, prs, static_info } = this.state.user_data
+
+    const packages = static_info.status !== 200? [] : static_info.data.packages
+
 
     const packages_with_data = packages.filter((pkg_name) => {
+      /*
       const bugs_cnt = bugs[pkg_name].length
       const pull_requests_cnt = pull_requests[pkg_name].length
       const updates_cnt = updates[pkg_name].length
       const overrides_cnt = overrides[pkg_name].length
       const orphan = orphans[pkg_name].orphanned? 1 : 0
+      */
+      const bugs_cnt = bzs.status !== 200? 0 : bzs.data[pkg_name].length
+      const pull_requests_cnt = prs.status !== 200? 0: prs.data[pkg_name].length
+      const updates_cnt = static_info.data.updates[pkg_name].length
+      const overrides_cnt = static_info.data.overrides[pkg_name].length
+      const orphans = static_info.data.orphans[pkg_name].orphanned? 1 : 0
 
-      return bugs_cnt + pull_requests_cnt + updates_cnt + overrides_cnt + orphan > 0
+      return bugs_cnt + pull_requests_cnt + updates_cnt + overrides_cnt + orphans > 0
     })
+
     const packages_grid = R.splitEvery(2, [...new Set(packages_with_data)]).map((pair) => (
       <div className="row py-md-4" key={pair[0]+pair[1]}>
-        {pair.map((package_name) => (
-          <div className="col-md-6 py-3 py-sm-3 py-md-0" key={package_name}>
-            <Widget title={package_name}
-                    bugs={bugs[package_name]}
-                    pull_requests={pull_requests[package_name]}
-                    updates={updates[package_name]}
-                    overrides={overrides[package_name]}
-                    orphan={orphans[package_name]}/>
+        {pair.map((pkg_name) => (
+          <div className="col-md-6 py-3 py-sm-3 py-md-0" key={pkg_name}>
+            <Widget title={pkg_name}
+                    bugs={bzs.status !== 200? [] : bzs.data[pkg_name]}
+                    pull_requests={prs.status !== 200? [] : prs.data[pkg_name]}
+                    updates={static_info.data.updates[pkg_name]}
+                    overrides={static_info.data.overrides[pkg_name]}
+                    orphan={static_info.data.orphans[pkg_name]}/>
           </div>
         ))}
       </div>
@@ -63,7 +88,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Masthead />
+        <Masthead bzsLoading={bzs.status !== 200} prsLoading={prs.status !== 200} siLoading={static_info.status !== 200} />
         <div className="bodycontent pb-3">
           <div className="subheader">
             <div className="container">
--- src/Masthead.js
+++ src/Masthead.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 
-function Masthead() {
-  return (
-    <div className="masthead py-2">
-      <div className="container px-0">
-        <div className="row no-gutters">
-          <div className="col-sm-6">
-            <img src={logo} alt='Fedora Packager Dashboard'/>
+class Masthead extends Component {
+  render() {
+    const si_spinner = this.props.siLoading? (<span><i class="fas fa-sync-alt fa-spin"></i> Loading Packages</span>) : null
+    const bzs_spinner = this.props.bzsLoading? (<span><i class="fas fa-sync-alt fa-spin"></i> Loading Bugs</span>) : null
+    const prs_spinner = this.props.prsLoading? (<span><i class="fas fa-sync-alt fa-spin"></i> Loading PRs</span>) : null
+
+    return (
+      <div className="masthead py-2">
+        <div className="container px-0">
+          <div className="row no-gutters">
+            <div className="col-sm-6">
+              <img src={logo} alt='Fedora Packager Dashboard'/>
+            </div>
+            <div className="col-auto">
+            </div>
+            <div className="col-sm-4">
+              {si_spinner}&nbsp;&nbsp;&nbsp;{bzs_spinner}&nbsp;&nbsp;&nbsp;{prs_spinner}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Masthead;
