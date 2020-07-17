af277d657c205939e67a1f89c7d8def986ab6339
Lukas Brabec <lbrabec@redhat.com>
2020-07-17 09:05:13 +0200

    include footer in dashboard loading page

--- src/components/DashboardLoading.js
+++ src/components/DashboardLoading.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react"
+import Footer from "./Footer"
 
 class DashboardLoading extends Component {
     render() {
       return (
-        <div className="appEntryContainer">
-          <h1>
-            <i className="fas fa-spinner fa-spin"></i>
-            {this.props.children}
-          </h1>
+        <div className="App">
+          <div className="appEntryContainer">
+            <h1>
+              <i className="fas fa-spinner fa-spin"></i>
+              {this.props.children}
+            </h1>
+          </div>
+          <Footer />
         </div>
       )
     }
