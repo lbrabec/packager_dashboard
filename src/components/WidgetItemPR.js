7ed10eb2c0c4b17b661914d676949d6168f6c9ef
Lukas Brabec <lbrabec@redhat.com>
2020-07-27 14:22:01 +0200

    adapt to new ci_status format for PRs

--- src/components/WidgetItemPR.js
+++ src/components/WidgetItemPR.js
@@ -9,6 +9,17 @@ import {
 } from "./WidgetLayout"
 import * as moment from "moment"
 
+const getBadge = (text, status) => {
+  if (!["success", "failure"].includes(status))
+    return null
+  else
+    return (
+      <WidgetBadge type={status === "success" ? "success" : "danger"}>
+        {text} {status === "success" ? <i className="fas fa-check-circle"></i> : <i className="fas fa-times-circle"></i>}
+      </WidgetBadge>
+    )
+}
+
 export class PR extends PureComponent {
   render() {
     const created = moment.utc(this.props.date_created)
@@ -24,11 +35,8 @@ export class PR extends PureComponent {
             {this.props.author} for {this.props.release}
           </WidgetSubTitle>
         </WidgetHead>
-        {this.props.ci_status === null ? null : (
-          <WidgetBadge type={this.props.ci_status === "success" ? "success" : "danger"}>
-            CI {this.props.ci_status}
-          </WidgetBadge>
-        )}
+        {getBadge("KojiCI", this.props.ci_status["simple-koji-ci"])}
+        {getBadge("Zuul", this.props.ci_status.Zuul)}
         <WidgetIconDetail icon="fa-comment-o" alt="Number of comments" color="text-muted">
           {this.props.comments}
         </WidgetIconDetail>
