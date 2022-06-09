1f1f38e3c2d87ba8bea69f9ac80884574a70e294
Lukas Brabec <lbrabec@redhat.com>
2022-06-09 09:32:40 +0200

    version alert fix

--- src/components/Alerts/VersionAlert.js
+++ src/components/Alerts/VersionAlert.js
@@ -14,7 +14,7 @@ class VersionAlert extends PureComponent {
               New version of Packager Dashboard is available.
               <span>
                 <button type="button" className="btn btn-success mr-2" aria-label="Update"
-                        onClick={() => window.location.assign(`${window.location.origin}/user/${this.props.fasuser}`)}>
+                        onClick={() => window.location.assign(`${window.location.origin}/dashboard${window.location.search}`)}>
                   Update
                 </button>
                 <button type="button" className="btn btn-danger" data-dismiss="alert" aria-label="Close">
